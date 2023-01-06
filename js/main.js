d16c9fbaed5080383f0c75a677ef371c3e640f87
rafaelspa <rsdepaula67@gmail.com>
2023-01-06 13:25:38 -0300

    Plus and times

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("Ola");
+document.querySelector('#btn-plus').addEventListener("click", plus);
+document.querySelector('#btn-times').addEventListener("click", times);
+
+function plus() {
+    const n1 = document.getElementById("number1").value;
+    const n2 = document.getElementById("number2").value;
+    let result = document.getElementById('result-box');
+    result.innerHTML = "";
+    result.innerHTML = (Number(n1) + Number(n2)).toString();
+}
+
+function times() {
+    const n1 = document.getElementById("number1").value;
+    const n2 = document.getElementById("number2").value;
+    let result = document.getElementById('result-box');
+    result.innerHTML = "";
+    result.innerHTML = (Number(n1) * Number(n2)).toString();
+}
