c174b9d7d2d2981b569b75f615cbcd070e87017f
rafaelspa <rsdepaula67@gmail.com>
2023-01-07 12:20:22 -0300

    Refactor

--- js/main.js
+++ js/main.js
@@ -1,28 +1,76 @@
-document.querySelector('#btn-plus').addEventListener("click", plus);
-document.querySelector('#btn-times').addEventListener("click", times);
-document.querySelector('#btn-clear').addEventListener("click", clear);
+// Remove input-error from #number2
 
-function plus() {
-    const n1 = document.getElementById("number1").value;
-    const n2 = document.getElementById("number2").value;
-    let result = document.getElementById('result-box');
-    result.innerHTML = "";
-    result.innerHTML = (Number(n1) + Number(n2)).toString();
-}
+document.querySelector("#number2").classList.remove("input-error");
 
-function times() {
-    const n1 = document.getElementById("number1").value;
-    const n2 = document.getElementById("number2").value;
-    let result = document.getElementById('result-box');
-    result.innerHTML = "";
-    result.innerHTML = (Number(n1) * Number(n2)).toString();
-}
 
-function clear() {
-    document.getElementById("number1").value = "";
-    document.getElementById("number2").value = "";
-    document.getElementById('result-box').innerHTML = "0";
-}
+// Plus function
+
+document.querySelector('#btn-plus').addEventListener("click", () => {
+    const numbers = document.querySelectorAll(".form-input");
+    const result = document.querySelector('#result-box');
+   
+    let sum = 0;
+    let flag = true;
+
+    const numbersArray = Array.from(numbers);
+
+    numbersArray.forEach(input => {
+        if(input.value == "") {
+            input.classList.add("input-error");
+            flag = false;
+        }
+        sum += Number(input.value);
+    });
+    
+    if (flag){
+        result.innerHTML = "";
+        result.innerHTML = sum.toString();
+    }
+});
+
+
+// Times function
+
+document.querySelector('#btn-times').addEventListener("click", () => {
+    const numbers = document.querySelectorAll(".form-input");
+    const result = document.querySelector('#result-box');
+   
+    let sum = 1;
+    let flag = true;
+
+    const numbersArray = Array.from(numbers);
+
+    numbersArray.forEach(input => {
+        if(input.value == "") {
+            input.classList.add("input-error");
+            flag = false;
+        }
+        sum *= Number(input.value);
+    });
+    
+    if (flag){
+        result.innerHTML = "";
+        result.innerHTML = sum.toString();
+    }
+});
+
+// Clear function
+
+document.querySelector('#btn-clear').addEventListener("click", () => {
+    const numbers = document.querySelectorAll(".form-input");
+
+    const numbersArray = Array.from(numbers);
+
+    numbersArray.forEach(input => {
+        input.value = "";
+        input.classList.remove("input-error");
+    });
+
+    document.getElementById('result-box').innerHTML = '\n              0\n            ';
+});
+
+
+// input error trigger when is not a number and block non number chars
 
 const inputList = Array.from(document.querySelectorAll('.form-input'));
 
@@ -36,7 +84,6 @@ inputList.forEach(input => {
         }
     });
 
-    
     input.addEventListener("keyup", element => {
         let n = Number(element.target.value);
         if (!isNumber(n)){
